b825f9a18220d545c4f7fb63aae00a6e83c1af78
stanc <stanko_uzunov@yahoo.com>
2022-11-17 20:02:49 +0100

    Add Encapsulation examples

---
+++ OopPrinciples/Encapsulations/Encapsulation.java
@@ -0,0 +1,8 @@
+public class Encapsulation {
+    public static void main(String[] args) {
+        Student mark = new Student("22MALA01");
+        student.setAge(27);
+
+        student.getAge();
+    }
+}
---
+++ OopPrinciples/Encapsulations/Student.java
@@ -0,0 +1,32 @@
+public class Student {
+
+    private String id;
+    private int age;
+    private String name;
+
+    public Student(String id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNameOfStudent() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        if(age > 18) {
+            this.age = age;
+        }
+    }
+
+    public void setNameOfStudent(String name) {
+        this.name = name;
+    }
+}
