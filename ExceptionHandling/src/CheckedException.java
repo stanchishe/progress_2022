73994062c92515c9971087e4975fcd341ccce5d7
stanc <stanko_uzunov@yahoo.com>
2022-11-22 20:08:24 +0100

    Fix spelling error in file name

--- ExceptionHandling/src/CheckedException.java
+++ ExceptionHandling/src/CheckedException.java
@@ -1,7 +1,7 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-public class CheckedExeption {
+public class CheckedException {
     public static void main(String[] args)
             throws FileNotFoundException
     {
